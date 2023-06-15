f1139ea9c5fc282a43bed39f790718771338ca48
Mina Kim <rmaomina@gmail.com>
2023-06-15 21:01:19 +0900

    [design] 호버시 배경 색상 변경

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -20,7 +20,7 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
   return (
     <div
       onClick={handleClick}
-      className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800.50 w-full p-2 rounded-md"
+      className="flex items-center w-full p-2 rounded-md cursor-pointer gap-x-3 hover:bg-neutral-800/50"
     >
       <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
         <Image
